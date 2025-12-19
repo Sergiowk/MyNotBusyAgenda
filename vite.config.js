e62d3086401549dfe0f06621a5d93d095b00e533
Sergio <34040046+Sergiowk@users.noreply.github.com>
2025-12-19 15:00:25 +0100

    Revert "Update vite.config.js"
    This reverts commit 8b542dab121a9ab1bf1bc8d6752f681b3f3073a2.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { execSync } from 'child_process'
 
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
@@ -10,7 +11,34 @@ export default defineConfig({
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['192.png', '512.png', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'MyNotBusyAgenda',
+        short_name: 'MyAgenda',
+        description: 'A personal organization app for a clear mind.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      }
+    })
   ],
   base: "/MyNotBusyAgenda/",
 })
