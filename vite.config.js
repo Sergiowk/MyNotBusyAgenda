8b542dab121a9ab1bf1bc8d6752f681b3f3073a2
Sergio <34040046+Sergiowk@users.noreply.github.com>
2025-12-19 14:42:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import { execSync } from 'child_process'
 
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
@@ -11,34 +10,7 @@ export default defineConfig({
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['192.png', '512.png', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'MyNotBusyAgenda',
-        short_name: 'MyAgenda',
-        description: 'A personal organization app for a clear mind.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '.',
-        icons: [
-          {
-            src: '192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          }
-        ]
-      }
-    })
+    react()
   ],
   base: "/MyNotBusyAgenda/",
 })
